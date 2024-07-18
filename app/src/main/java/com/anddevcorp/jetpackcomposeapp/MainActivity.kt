fb9c87c616cd71684fda0c61793f34ac1dbfaede
si0044 <suleyman.irmak@fups.com>
2024-07-18 17:54:52 +0300

    composition and recomposition

--- app/src/main/java/com/anddevcorp/jetpackcomposeapp/MainActivity.kt
+++ app/src/main/java/com/anddevcorp/jetpackcomposeapp/MainActivity.kt
@@ -1,39 +1,28 @@
 package com.anddevcorp.jetpackcomposeapp
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.GridItemSpan
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.itemsIndexed
-import androidx.compose.foundation.lazy.grid.rememberLazyGridState
-import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.SideEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -41,20 +30,15 @@ import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.anddevcorp.jetpackcomposeapp.data.request.getRequestModel
 import com.anddevcorp.jetpackcomposeapp.model.Movie
 import com.anddevcorp.jetpackcomposeapp.model.MovieUiState
 import com.anddevcorp.jetpackcomposeapp.ui.ext.GetImage
 import com.anddevcorp.jetpackcomposeapp.ui.ext.customBorderShape
 import com.anddevcorp.jetpackcomposeapp.ui.ext.customShape
+import com.anddevcorp.jetpackcomposeapp.ui.theme.MyTryAppTheme
 import com.theapache64.rebugger.Rebugger
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.coroutineScope
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
@@ -63,21 +47,9 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-
-//            MovieListScreen()
-            MovieScreen(
-                onClick = { index ->
-                    viewModel.changeData(index)
-                }
-            )
-//            viewModel.getMovies(getRequestModel())
-//            val people = viewModel.peopleList
-//            ListScreen(
-//                people = people,
-//                onItemClick = {
-//                    viewModel.toggleSelection(it)
-//                }
-//            )
+            MyTryAppTheme {
+                MovieScreen()
+            }
         }
     }
 }
@@ -85,7 +57,6 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun MovieScreen(
     viewModel: MovieViewModel = androidx.lifecycle.viewmodel.compose.viewModel(),
-    onClick: (Int) -> Unit
 ) {
     LaunchedEffect(Unit) {
         viewModel.getMovies(getRequestModel())
@@ -100,7 +71,9 @@ fun MovieScreen(
         when (movieUiState) {
             is MovieUiState.Success -> {
                 val movies = (movieUiState as MovieUiState.Success).movies
-                MovieList(movies = movies, onClick = onClick)
+                MovieList(movies = movies, onClick = { index ->
+                    viewModel.changeData(index)
+                })
             }
 
             MovieUiState.Empty -> {
@@ -127,11 +100,9 @@ fun MovieScreen(
 }
 
 @Composable
-fun MovieItem(movie: Movie, position:Int, onClick: (Int) -> Unit) {
+fun MovieItem(movie: Movie, position: Int, onClick: (Int) -> Unit) {
     Rebugger(trackMap = mapOf("movie" to movie, "onClick" to onClick))
-    SideEffect {
-        Log.d("MyComposable", "Recomposed!")
-    }
+
     Box(
         modifier = Modifier
             .padding(5.dp)
@@ -152,68 +123,12 @@ fun MovieItem(movie: Movie, position:Int, onClick: (Int) -> Unit) {
     }
 }
 
-//
-//@Composable
-//fun MovieItem(movie: Movie, onClick: () -> Unit) {
-//    Rebugger(mapOf("movie" to movie, "onClick" to onClick))
-//    var isSelected by remember { mutableStateOf(false) }
-//
-//    Box(
-//        modifier = Modifier
-//            .padding(5.dp)
-//            .clickable {
-//                isSelected = !isSelected
-//                onClick()
-//            }
-//            .border(2.dp, Color.Black, customBorderShape)
-//            .background(Color.White, customShape)
-//            .height(200.dp)
-//            .clip(customShape)
-//    ) {
-//        AnimatedVisibility(
-//            visible = false, // isSelected,
-//            enter = slideInVertically(initialOffsetY = { it }),
-//            exit = slideOutVertically(targetOffsetY = { if (isSelected) -it else it })
-//        ) {
-//            Column(
-//                modifier = Modifier
-//                    .animateContentSize()
-//                    .padding(5.dp)
-//                    .verticalScroll(rememberScrollState())
-//            ) {
-//                Text(
-//                    modifier = Modifier.align(Alignment.CenterHorizontally),
-//                    text = movie.originalTitle,
-//                    style = TextStyle(
-//                        color = Color.Red,
-//                        fontWeight = FontWeight.Bold
-//                    )
-//                )
-//                Text(text = movie.overview)
-//            }
-//        }
-//        AnimatedVisibility(
-//            visible = true , //!isSelected,
-//            enter = slideInVertically(initialOffsetY = { if (isSelected) it else -it }),
-//            exit = slideOutVertically(targetOffsetY = { -it })
-//        ) {
-//            GetImage(
-//                movie.posterPath,
-//                modifier = Modifier
-//                    .align(Alignment.Center)
-//                    .fillMaxSize()
-//                    .scale(1.8f)
-//                    .aspectRatio(1f)
-//            )
-//        }
-//    }
-//}
-
 @Composable
 fun MovieList(
     movies: List<Movie>,
     onClick: (Int) -> Unit
 ) {
+
     LazyVerticalGrid(
         columns = GridCells.Fixed(3),
         contentPadding = PaddingValues(10.dp),
@@ -221,115 +136,10 @@ fun MovieList(
         itemsIndexed(
             movies,
             key = { _, movie -> movie.id },
-            span = { _, _ -> GridItemSpan(1) }) { index, movie ->
+            span = { _, _ -> GridItemSpan(1) }
+        ) { index, movie ->
             MovieItem(movie = movie, position = index, onClick = onClick)
         }
     }
 }
 
-@Composable
-fun ListScreen(people: List<Movie>, onItemClick: (Int) -> Unit) {
-    Rebugger(trackMap = mapOf("movie" to people, "onClick" to onItemClick))
-    LazyColumn(
-        verticalArrangement = Arrangement.spacedBy(2.dp),
-        modifier = Modifier.fillMaxSize()
-    ) {
-        itemsIndexed(items = people, key = { index, item ->  item.id }) {index, it ->
-            ListItem(item = it, onItemClick = {onItemClick(index)})
-        }
-    }
-}
-
-@Composable
-fun ListItem(item: Movie, onItemClick: () -> Unit) {
-    Column(
-        modifier = Modifier.border(3.dp, Color.Black)
-    ) {
-        Box(
-            modifier = Modifier
-                .fillMaxWidth()
-                .clickable {
-                    onItemClick()
-                }
-                .padding(8.dp)
-        ) {
-            Text("Index: Name ${item.title}", fontSize = 20.sp)
-            GetImage(
-                imageUrl = item.posterPath,
-                modifier = Modifier
-                    .align(Alignment.Center)
-                    .height(50.dp)
-                    .width(50.dp)
-                    .scale(1.8f)
-                    .aspectRatio(1f)
-            )
-        }
-    }
-}
-
-//// recomposition error
-//@Composable
-//fun MovieItem(movie: Movie, change: Boolean, onClick: () -> Unit ) {
-//
-//    var isSelected by remember { mutableStateOf(false) }
-//
-//    Box(
-//        modifier = Modifier
-//            .padding(5.dp)
-//            .clickable {
-//                isSelected = !change
-//                onClick()
-//            }
-//            .border(2.dp, Color.Black, customBorderShape)
-//            .background(Color.White, customShape)
-//            .height(200.dp)
-//            .clip(customShape)
-//    ) {
-//        AnimatedVisibility(
-//            visible = isSelected,
-//            enter = slideInVertically(initialOffsetY = { it }),
-//            exit =  slideOutVertically(targetOffsetY = { if (isSelected) -it else it })
-//        ) {
-//            Column(
-//                modifier = Modifier
-//                    .animateContentSize()
-//                    .padding(5.dp)
-//            ) {
-//                Text(text = movie.originalTitle)
-//                Text(text = movie.overview)
-//            }
-//        }
-//        AnimatedVisibility(
-//            visible = !isSelected,
-//            enter = slideInVertically(initialOffsetY = { if (isSelected) it else -it }),
-//            exit = slideOutVertically(targetOffsetY = { -it })
-//        ) {
-//            GetImage(
-//                movie.posterPath,
-//                modifier = Modifier
-//                    .align(Alignment.Center)
-//                    .fillMaxSize()
-//                    .scale(1.8f)
-//                    .aspectRatio(1f)
-//            )
-//        }
-//    }
-//}
-//
-//
-//@Composable
-//fun MovieList(movies: List<Movie>) {
-//    var change by remember {
-//        mutableStateOf(false)
-//    }
-//    LazyVerticalGrid(
-//        columns = GridCells.Fixed(3),
-//        contentPadding = PaddingValues(10.dp),
-//    ) {
-//        items(movies) { movie ->
-//            MovieItem(movie = movie, change, onClick = {
-//                change = true
-//            })
-//        }
-//    }
-//}
--- app/src/main/java/com/anddevcorp/jetpackcomposeapp/MovieViewModel.kt
+++ app/src/main/java/com/anddevcorp/jetpackcomposeapp/MovieViewModel.kt
@@ -1,17 +1,11 @@
 package com.anddevcorp.jetpackcomposeapp
 
-import androidx.compose.runtime.Stable
-import androidx.compose.runtime.mutableStateListOf
-import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.compose.runtime.toMutableStateList
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.anddevcorp.jetpackcomposeapp.data.repository.MovieRepositoryImpl
 import com.anddevcorp.jetpackcomposeapp.model.MovieUiState
 import com.anddevcorp.jetpackcomposeapp.data.request.MovieRequest
-import com.anddevcorp.jetpackcomposeapp.data.request.getRequestModel
 import com.anddevcorp.jetpackcomposeapp.data.usecase.MovieUseCase
-import com.anddevcorp.jetpackcomposeapp.model.Movie
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
@@ -21,12 +15,10 @@ import javax.inject.Inject
 @HiltViewModel
 class MovieViewModel @Inject constructor(
     private val movieUseCase: MovieUseCase,
-    private val movieRepositoryImpl: MovieRepositoryImpl
 ) : ViewModel() {
 
     private val _movies = MutableStateFlow<MovieUiState>(MovieUiState.Loading)
     val movies = _movies.asStateFlow()
-
 
     fun getMovies(request: MovieRequest) {
         viewModelScope.launch {
@@ -43,305 +35,16 @@ class MovieViewModel @Inject constructor(
             }
         }
     }
+
     fun changeData(movieIndex: Int) {
         viewModelScope.launch {
             val currentState = _movies.value
             if (currentState is MovieUiState.Success) {
-                var movies = currentState.movies[movieIndex]
-                currentState.movies[movieIndex] = movies.copy(posterPath = currentState.movies[movieIndex].backdropPath)
-//                val movies = currentState.movies
-//                val movie = movies[movieIndex]
-//                movies[movieIndex] = movie.copy(posterPath = movie.backdropPath)
-//                _movies.value = MovieUiState.Success(movies)
+                val updatedMovies = currentState.movies.apply {
+                    this[movieIndex] = this[movieIndex].copy(posterPath = this[movieIndex].backdropPath)
+                }
+                _movies.emit(MovieUiState.Success(updatedMovies))
             }
         }
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//    private val initialList = listOf(
-//        Person(
-//            adult = false,
-//            backdropPath = "/vpnVM9B6NMmQpWeZvzLvDESb2QY.jpg",
-//            id = 0,
-//            originalLanguage = "en",
-//            originalTitle = "Original Title 1",
-//            overview = "This is a dummy overview for movie 1.",
-//            popularity = 10.0,
-//            posterPath = "/3aYym07c0oJjg6h00ras1SCPKt9.jpg",
-//            releaseDate = "2023-01-01",
-//            title = "Dummy Movie 1",
-//            video = false,
-//            voteAverage = 7.5,
-//            voteCount = 100
-//        ),
-//        Person(
-//            adult = false,
-//            backdropPath = "/vpnVM9B6NMmQpWeZvzLvDESb2QY.jpg",
-//            id = 1,
-//            originalLanguage = "en",
-//            originalTitle = "Original Title 2",
-//            overview = "This is a dummy overview for movie 2.",
-//            popularity = 20.0,
-//            posterPath = "/3aYym07c0oJjg6h00ras1SCPKt9.jpg",
-//            releaseDate = "2023-02-01",
-//            title = "Dummy Movie 2",
-//            video = false,
-//            voteAverage = 7.0,
-//            voteCount = 150
-//        ),
-//        Person(
-//            adult = false,
-//            backdropPath = "/vpnVM9B6NMmQpWeZvzLvDESb2QY.jpg",
-//            id = 2,
-//            originalLanguage = "en",
-//            originalTitle = "Original Title 3",
-//            overview = "This is a dummy overview for movie 3.",
-//            popularity = 30.0,
-//            posterPath = "/3aYym07c0oJjg6h00ras1SCPKt9.jpg",
-//            releaseDate = "2023-03-01",
-//            title = "Dummy Movie 3",
-//            video = false,
-//            voteAverage = 6.5,
-//            voteCount = 200
-//        ),
-//        Person(
-//            adult = false,
-//            backdropPath = "/vpnVM9B6NMmQpWeZvzLvDESb2QY.jpg",
-//            id = 3,
-//            originalLanguage = "en",
-//            originalTitle = "Original Title 4",
-//            overview = "This is a dummy overview for movie 4.",
-//            popularity = 40.0,
-//            posterPath = "/3aYym07c0oJjg6h00ras1SCPKt9.jpg",
-//            releaseDate = "2023-04-01",
-//            title = "Dummy Movie 4",
-//            video = false,
-//            voteAverage = 8.0,
-//            voteCount = 250
-//        ),
-//        Person(
-//            adult = false,
-//            backdropPath = "/vpnVM9B6NMmQpWeZvzLvDESb2QY.jpg",
-//            id = 4,
-//            originalLanguage = "en",
-//            originalTitle = "Original Title 5",
-//            overview = "This is a dummy overview for movie 5.",
-//            popularity = 50.0,
-//            posterPath = "/3aYym07c0oJjg6h00ras1SCPKt9.jpg",
-//            releaseDate = "2023-05-01",
-//            title = "Dummy Movie 5",
-//            video = false,
-//            voteAverage = 7.2,
-//            voteCount = 300
-//        ),
-//        Person(
-//            adult = false,
-//            backdropPath = "/vpnVM9B6NMmQpWeZvzLvDESb2QY.jpg",
-//            id = 5,
-//            originalLanguage = "en",
-//            originalTitle = "Original Title 6",
-//            overview = "This is a dummy overview for movie 6.",
-//            popularity = 60.0,
-//            posterPath = "/vpnVM9B6NMmQpWeZvzLvDESb2QY.jpg",
-//            releaseDate = "2023-06-01",
-//            title = "Dummy Movie 6",
-//            video = false,
-//            voteAverage = 8.5,
-//            voteCount = 350
-//        ),
-//        Person(
-//            adult = false,
-//            backdropPath = "/vpnVM9B6NMmQpWeZvzLvDESb2QY.jpg",
-//            id = 6,
-//            originalLanguage = "en",
-//            originalTitle = "Original Title 7",
-//            overview = "This is a dummy overview for movie 7.",
-//            popularity = 70.0,
-//            posterPath = "/vpnVM9B6NMmQpWeZvzLvDESb2QY.jpg",
-//            releaseDate = "2023-07-01",
-//            title = "Dummy Movie 7",
-//            video = false,
-//            voteAverage = 6.8,
-//            voteCount = 400
-//        ),
-//        Person(
-//            adult = false,
-//            backdropPath = "/vpnVM9B6NMmQpWeZvzLvDESb2QY.jpg",
-//            id = 7,
-//            originalLanguage = "en",
-//            originalTitle = "Original Title 8",
-//            overview = "This is a dummy overview for movie 8.",
-//            popularity = 80.0,
-//            posterPath = "/vpnVM9B6NMmQpWeZvzLvDESb2QY.jpg",
-//            releaseDate = "2023-08-01",
-//            title = "Dummy Movie 8",
-//            video = false,
-//            voteAverage = 9.0,
-//            voteCount = 450
-//        ),
-//        Person(
-//            adult = false,
-//            backdropPath = "/vpnVM9B6NMmQpWeZvzLvDESb2QY.jpg",
-//            id = 8,
-//            originalLanguage = "en",
-//            originalTitle = "Original Title 9",
-//            overview = "This is a dummy overview for movie 9.",
-//            popularity = 90.0,
-//            posterPath = "/vpnVM9B6NMmQpWeZvzLvDESb2QY.jpg",
-//            releaseDate = "2023-09-01",
-//            title = "Dummy Movie 9",
-//            video = false,
-//            voteAverage = 7.9,
-//            voteCount = 500
-//        ),
-//        Person(
-//            adult = false,
-//            backdropPath = "/vpnVM9B6NMmQpWeZvzLvDESb2QY.jpg",
-//            id = 9,
-//            originalLanguage = "en",
-//            originalTitle = "Original Title 10",
-//            overview = "This is a dummy overview for movie 10.",
-//            popularity = 100.0,
-//            posterPath = "/vpnVM9B6NMmQpWeZvzLvDESb2QY.jpg",
-//            releaseDate = "2023-10-01",
-//            title = "Dummy Movie 10",
-//            video = false,
-//            voteAverage = 8.1,
-//            voteCount = 550
-//        ),
-//        Person(
-//            adult = false,
-//            backdropPath = "/vpnVM9B6NMmQpWeZvzLvDESb2QY.jpg",
-//            id = 10,
-//            originalLanguage = "en",
-//            originalTitle = "Original Title 11",
-//            overview = "This is a dummy overview for movie 11.",
-//            popularity = 110.0,
-//            posterPath = "/vpnVM9B6NMmQpWeZvzLvDESb2QY.jpg",
-//            releaseDate = "2023-11-01",
-//            title = "Dummy Movie 11",
-//            video = false,
-//            voteAverage = 6.9,
-//            voteCount = 600
-//        ),
-//        Person(
-//            adult = false,
-//            backdropPath = "/vpnVM9B6NMmQpWeZvzLvDESb2QY.jpg",
-//            id = 11,
-//            originalLanguage = "en",
-//            originalTitle = "Original Title 12",
-//            overview = "This is a dummy overview for movie 12.",
-//            popularity = 120.0,
-//            posterPath = "/vpnVM9B6NMmQpWeZvzLvDESb2QY.jpg",
-//            releaseDate = "2023-12-01",
-//            title = "Dummy Movie 12",
-//            video = false,
-//            voteAverage = 8.3,
-//            voteCount = 650
-//        ),
-//        Person(
-//            adult = false,
-//            backdropPath = "/vpnVM9B6NMmQpWeZvzLvDESb2QY.jpg",
-//            id = 12,
-//            originalLanguage = "en",
-//            originalTitle = "Original Title 13",
-//            overview = "This is a dummy overview for movie 13.",
-//            popularity = 130.0,
-//            posterPath = "/vpnVM9B6NMmQpWeZvzLvDESb2QY.jpg",
-//            releaseDate = "2024-01-01",
-//            title = "Dummy Movie 13",
-//            video = false,
-//            voteAverage = 7.4,
-//            voteCount = 700
-//        ),
-//        Person(
-//            adult = false,
-//            backdropPath = "/vpnVM9B6NMmQpWeZvzLvDESb2QY.jpg",
-//            id = 13,
-//            originalLanguage = "en",
-//            originalTitle = "Original Title 14",
-//            overview = "This is a dummy overview for movie 14.",
-//            popularity = 140.0,
-//            posterPath = "/vpnVM9B6NMmQpWeZvzLvDESb2QY.jpg",
-//            releaseDate = "2024-02-01",
-//            title = "Dummy Movie 14",
-//            video = false,
-//            voteAverage = 8.7,
-//            voteCount = 750
-//        ),
-//        Person(
-//            adult = false,
-//            backdropPath = "/vpnVM9B6NMmQpWeZvzLvDESb2QY.jpg",
-//            id = 14,
-//            originalLanguage = "en",
-//            originalTitle = "Original Title 15",
-//            overview = "This is a dummy overview for movie 15.",
-//            popularity = 150.0,
-//            posterPath = "/3aYym07c0oJjg6h00ras1SCPKt9.jpg",
-//            releaseDate = "2024-03-01",
-//            title = "Dummy Movie 15",
-//            video = false,
-//            voteAverage = 7.8,
-//            voteCount = 800
-//        )
-//    )
-
-//    var peopleList = mutableStateListOf<Movie>().apply {
-////        addAll(initialList)
-//        addAll(initial2)
-//    }
-
-//    fun toggleSelection(index: Int) {
-////        val item = peopleList[index]
-////        peopleList[index] = item.copy(posterPath = item.backdropPath)
-//        val list = people
-//        people.forEachIndexed {i, item ->
-//            if (i == index) {
-//                people[i] = item.copy(posterPath = item.backdropPath)
-//            }
-//        }
-//    }
-//
-////    fun toggleSelectionPeople(index: Int) {
-////        val item = people[index]
-////        people[index] = item.copy(posterPath = item.backdropPath)
-////    }
-
-
 }
-
-//data class Person(
-//    val adult: Boolean = false,
-//    val backdropPath: String,
-//    val id: Int,
-//    val originalLanguage: String,
-//    val originalTitle: String,
-//    val overview: String,
-//    val popularity: Double,
-//    val posterPath: String,
-//    val releaseDate: String,
-//    val title: String,
-//    val video: Boolean,
-//    val voteAverage: Double,
-//    val voteCount: Int,
-//)
